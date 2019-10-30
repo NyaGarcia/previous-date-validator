fb847d5512cc6671bc15f0e9178b88f13357615a
Nya Garcia <nyablk97@gmail.com>
2019-10-30 10:48:09 +0100

    refactor(validator): use as casting syntax instead of <> syntax

--- src/validator.ts
+++ src/validator.ts
@@ -3,8 +3,6 @@ import {
   parseMessageWithCustomArgs,
 } from '@lemoncode/fonk';
 
-import { DEFAULT_ENCODING } from 'crypto';
-
 // TODO: Add validator type
 const VALIDATOR_TYPE = 'PREVIOUS_DATE';
 
@@ -18,26 +16,22 @@ const DEFAULT_PARAMS: CustomArgs = {
 
 const BAD_PARAMETER = 'Value must be a valid Date object';
 
-// TODO: Add default message
 let defaultMessage = "Date isn't previous to the one provided";
 export const setErrorMessage = message => (defaultMessage = message);
 
 const isDefined = value => value !== void 0 && value !== null && value !== '';
 
-export const validator: FieldValidationFunctionSync = fieldValidatorArgs => {
-  const {
-    value,
-    message = defaultMessage,
-    customArgs = DEFAULT_PARAMS as CustomArgs,
-  } = fieldValidatorArgs;
-  const { date } = <CustomArgs>customArgs;
+export const validator: FieldValidationFunctionSync = ({
+  value,
+  message = defaultMessage,
+  customArgs = DEFAULT_PARAMS,
+}) => {
+  const { date } = customArgs as CustomArgs;
 
-  if (!(value instanceof Date)) {
+  if (!(value instanceof Date) || !(date instanceof Date)) {
     throw new TypeError(BAD_PARAMETER);
   }
-  console.log({ value });
-  console.log({ date });
-  // TODO: Add validator
+
   const succeeded = !isDefined(value) || value < date;
 
   return {
