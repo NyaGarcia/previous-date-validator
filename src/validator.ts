5f57ce65eb45ed6a773bbaac7fd76b96782b5b83
Nya Garcia <nyablk97@gmail.com>
2019-10-31 20:39:17 +0100

    refactor(validator): add guard clause and remove DEFAULT_PARAMS

--- src/validator.ts
+++ src/validator.ts
@@ -9,38 +9,35 @@ export interface CustomArgs {
   date: Date;
 }
 
-const DEFAULT_PARAMS: CustomArgs = {
-  date: new Date(),
-};
+const MISSING_ARGS =
+  'FieldValidationError: date option for date validation is mandatory. Example: { date: new Date() }.';
+const BAD_PARAMETER = 'Value must be a valid Date object.';
 
-const BAD_PARAMETER = 'Value must be a valid Date object';
-
-let defaultMessage = "Date isn't previous to the one provided";
+let defaultMessage = "Date isn't previous to the one provided.";
 export const setErrorMessage = message => (defaultMessage = message);
-
-const isDefined = value => value !== void 0 && value !== null && value !== '';
 
 export const validator: FieldValidationFunctionSync<CustomArgs> = ({
   value,
   message = defaultMessage,
-  customArgs = DEFAULT_PARAMS,
+  customArgs,
 }) => {
+  if (!customArgs) {
+    throw new Error(MISSING_ARGS);
+  }
+
   const { date } = customArgs;
 
-  if (!(value instanceof Date) || !(date instanceof Date)) {
+  if (!(value instanceof Date)) {
     throw new TypeError(BAD_PARAMETER);
   }
 
-  const succeeded = !isDefined(value) || value < date;
+  const succeeded = value < date;
 
   return {
     succeeded,
     message: succeeded
       ? ''
-      : parseMessageWithCustomArgs(
-          (message as string) || defaultMessage,
-          customArgs
-        ),
+      : parseMessageWithCustomArgs(message as string, customArgs),
     type: VALIDATOR_TYPE,
   };
 };
